cdda4968304ec6d938fa4d996e2d093b7ad4145b
Rahul <rkc.98@outlook.com>
2021-10-16 21:31:43 +0530

    root commit

---
+++ src/main/java/com/spring/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.spring.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String displayHome() {
+		return "index";
+	}
+	
+	@RequestMapping("/display")
+	public String displayPage() {
+		return "display";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+<title></title></head>
+
+<body>
+
+<h1>
+home is working</h1>
+
+<h2>
+through the controller
+</h2>
+</body>
+</html>
